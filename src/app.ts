8a9d14f115aff18325a75021a32eb1a3043e0f7d
Asif-Zaman-Suvo <asifshuvo119181@gmail.com>
2024-07-04 16:20:11 +0600

    link edited

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const app: Application = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: 'smartment-management-frontend-suvo.vercel.app',
+    origin: 'https://smartphone-management-frontend-suvo.vercel.app',
     // origin: 'http://localhost:5173',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
